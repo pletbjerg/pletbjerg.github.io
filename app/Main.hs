d509bf83a8a2ac56e7b072310b3f161fc08e9753
christian <>
2022-06-19 21:08:02 -0600

    humble beginnings

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Hakyll
+import Hakyll 
+    ( Configuration (deployCommand, destinationDirectory)
+    )
+import qualified Hakyll
 
+-- | Configuration for the main hakyll call.
+conf :: Configuration 
+conf = Hakyll.defaultConfiguration
+    { deployCommand = "git add docs && git commit 'Deployment' && git push"
+    , destinationDirectory = "docs/"
+    }
+
+-- | main function
 main :: IO ()
-main = hakyll $ do
-    create ["archive.html"] $ do
-        route idRoute
+main = Hakyll.hakyllWith conf $ do
+    -- posts.
+    Hakyll.match "posts/**/*.tex" $ do
+        Hakyll.route $ Hakyll.setExtension "html"
+        Hakyll.compile $ Hakyll.pandocCompiler 
+
+    -- index.html
+    Hakyll.create ["index.html"] $ do
+        Hakyll.route $ Hakyll.constRoute "index.html"
+        Hakyll.compile $ Hakyll.makeItem ("wahoo"  :: String)
+
+
+            
+    {-
+    Hakyll.create ["archive.html"] $ do
+        Hakyll.route Hakyll.idRoute
+    -}
     
