dcc6bf791d24a8aea82381da5f133aa4221d0187
christian <>
2023-07-23 23:59:59 -0600

    Added 'PandocShake' monad so underlying file dependencies from pandoc are tracked via shake

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
-import Development.Shake
-import Development.Shake.FilePath
-
+-- Prelude / base
 import Control.Arrow
 import Control.Monad
 import Control.Monad.Except
@@ -15,37 +13,73 @@ import Data.Maybe
 import Data.List 
 import Data.Ord 
 import Data.Foldable 
+import Data.Traversable 
+import System.Environment
+import Debug.Trace
 
+-- shake
+import Development.Shake
+import Development.Shake.FilePath
+
+-- Text / bytestring
 import Data.Text (Text)
 import qualified Data.Text as Text
 
-import Text.Pandoc (Pandoc (..), MetaValue (..), ReaderOptions (..), WriterOptions (..), Template)
-import Text.Pandoc.Options ( HTMLMathMethod (MathJax))
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+
+-- Pandoc
+import Text.Pandoc 
+    ( Pandoc (..)
+    , MetaValue (..)
+    , ReaderOptions (..)
+    , WriterOptions (..)
+    , Template
+    , PandocError
+    , PandocMonad
+    )
+import Text.Pandoc.Options ( HTMLMathMethod (MathJax), CiteMethod (Citeproc) )
+import Text.Pandoc.Class ( PandocIO (unPandocIO), CommonState (..) )
 import qualified Text.Pandoc.Templates as Pandoc
 import qualified Text.Pandoc.Definition as Pandoc
 import qualified Text.Pandoc.Options as Pandoc
 import qualified Text.Pandoc.Class as Pandoc
 import qualified Text.Pandoc.UTF8 as Pandoc
+import qualified Text.Pandoc.Citeproc as Pandoc
 import qualified Text.Pandoc.Highlighting as Pandoc
 import qualified Text.Pandoc.Readers.LaTeX as Pandoc
+import qualified Text.Pandoc.Readers.BibTeX as Pandoc
 import qualified Text.Pandoc.Writers as Pandoc
 import qualified Text.Pandoc.Shared as Pandoc
 import qualified Text.Pandoc.Writers.Shared as Pandoc
+import qualified Text.Pandoc.Error as Pandoc
 
+-- Pandocs templates
 import qualified Text.DocTemplates as DocTemplates
 import qualified Text.DocLayout as DocLayout
 
+-- Monads
+import Control.Monad.Except (ExceptT, MonadError)
+import qualified Control.Monad.Except as Except
+import Control.Monad.State (StateT, MonadState)
+import qualified Control.Monad.State as State
+import Control.Monad.Reader (ReaderT, MonadReader)
+import qualified Control.Monad.Reader as Reader
+import qualified Control.Monad.Trans as Trans
+-- TODO: perhaps file an issue on shake s.t. they include these instances in
+-- the 'Action' monad
+-- import Control.Monad.Catch (MonadCatch, MonadMask, MonadThrow)
+-- import qualified Control.Monad.Catch as Catch
+
+-- Json
 import Data.Aeson (Value (..))
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.KeyMap as Aeson
 
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as ByteString
-
+-- Time / date parsing
 import Data.Time (Day)
 import qualified Data.Time as Time
 
-import Debug.Trace
 
 -- | 'defaultReaderOpts' are the default reader options for pandocs.
 defaultReaderOpts :: ReaderOptions
@@ -73,7 +107,7 @@ compileTemplate ::
     -- | Compiled 'Template'
     Action (Template Text)
 compileTemplate templatePath = runTemplateAction $ do
-    liftAction $ need [templatePath]
+    liftActionToTemplateAction $ need [templatePath]
     templateContents <- liftIO $ Pandoc.runIOorExplode $ Pandoc.getTemplate templatePath 
     eitherTemplate <- Pandoc.compileTemplate templatePath templateContents 
     liftIO $ liftEither $ Pandoc.mapLeft userError eitherTemplate
@@ -85,13 +119,55 @@ compileTemplate templatePath = runTemplateAction $ do
 --
 -- N.B. this automatically calls 'need' on the given 'FilePath'
 readLaTeXAndWriteHtml5String :: ReaderOptions -> WriterOptions -> FilePath -> Action (Text, Pandoc)
-readLaTeXAndWriteHtml5String readerOpts writerOpts filePath = do
-    need [filePath]
-    liftIO $ Pandoc.runIOorExplode $ do
+readLaTeXAndWriteHtml5String readerOpts writerOpts filePath = 
+    readLaTeXWithBibAndWriteHtml5String readerOpts writerOpts filePath []
+
+-- | @'readLaTeXWithBibAndWriteHtml5String' readerOpts writerOpts filePath
+-- bibsFilePaths@ reads the LaTeX file @filePath@ alongside with all provided
+-- bibliography files @bibsFilePaths@; and converts it into the corresponding
+-- HTML5 string via pandoc using the given @readerOpts@ and @writerOpts@ and
+-- returns the internal 'Pandoc' representation.
+--
+-- N.B. this automatically calls 'need' on all the given 'FilePath'
+readLaTeXWithBibAndWriteHtml5String :: ReaderOptions -> WriterOptions -> FilePath -> [FilePath] -> Action (Text, Pandoc)
+readLaTeXWithBibAndWriteHtml5String readerOpts writerOpts filePath bibsFilePaths = do
+    need $ [filePath] ++ bibsFilePaths
+    let texDir = dropFileName filePath
+        -- reading the source code, @TEXINPUTS@ is the environment variable of
+        -- colon (":") seperated direectories that pandocs goes looking for when
+        -- searching for @\input{<file>}@, etc from a LaTeX file.
+        -- So, we add the current directory of the thing we wish to compile
+        -- there.
+        magicTEXINPUTSEnvVar = "TEXINPUTS"
+        newEnvironment = [(magicTEXINPUTSEnvVar, Text.pack texDir)]
+    environment <- fmap (map (Text.pack *** Text.pack)) $ liftIO getEnvironment 
+    -- TODO: perhaps instead of overriding the @TEXINPUTS@ environment variable
+    -- (internally, this uses 'lookup' which finds the _first_ key value pair),
+    -- we should really augment the current environment if it exists.
+    runPandocShakeWithEnvOrExplode (newEnvironment ++ environment) $ do
+        -- Include the directory of @filePath@ in the resource lookup path
+        Pandoc.modifyCommonState $ \commonState -> 
+            commonState { stResourcePath = stResourcePath commonState ++ [texDir]}
+
+        -- compile the LaTeX document to Pandoc
         fileContents <- liftIO $ Pandoc.readFile filePath
         pandoc <- Pandoc.readLaTeX readerOpts fileContents
-        htmlPost <- Pandoc.writeHtml5String writerOpts pandoc
-        return (htmlPost, pandoc)
+
+        -- compile the bib to Pandoc (note this only contains the bibilography
+        -- metadata
+        bib <- fmap fold $ for bibsFilePaths $ \bibFilePath -> do
+            bibFileContents <- liftIO $ Pandoc.readFile bibFilePath
+            Pandoc.readBibTeX readerOpts bibFileContents
+
+        -- add the biblographies metadata to the @pandoc@, and ensure that we
+        -- have the citations printed out in the body with the CiteProcfilter
+        let pandoc' = pandoc <> bib :: Pandoc
+        pandoc'' <- Pandoc.processCitations pandoc'
+
+        -- create the HTML post
+        htmlPost <- Pandoc.writeHtml5String writerOpts pandoc''
+
+        return (htmlPost, pandoc'')
 
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles = "docs"} $ do 
@@ -191,9 +267,16 @@ main = shakeArgs shakeOptions{shakeFiles = "docs"} $ do
     -- intermediate representation. 
     --
     -- Note we match on both the HTML *and* the pandoc JSON file.
+    --
+    -- TODO: break up the html and json rule into seperate rules, then we can
+    -- add pdfs in as well
     ["docs/posts/*/*.html", "docs/posts/*/*.json"] &%> \[htmlPostPath, jsonPostPath] -> do
         let texPostPath = dropDirectory1 htmlPostPath -<.> "tex"
             templatePath = "templates/post.html"
+
+        -- Grab the bib files in the same directory as the tex file we are
+        -- compiling.
+        bibsFilePaths <- getDirectoryFiles ""  [dropFileName texPostPath </> "*.bib"]
 
         template <- compileTemplate templatePath
 
@@ -202,7 +285,8 @@ main = shakeArgs shakeOptions{shakeFiles = "docs"} $ do
                 { writerTemplate = Just template
                 }
 
-        (htmlPost, htmlPandoc) <- readLaTeXAndWriteHtml5String readerOpts writerOpts texPostPath
+        (htmlPost, htmlPandoc) <- readLaTeXWithBibAndWriteHtml5String
+            readerOpts writerOpts texPostPath bibsFilePaths
 
         void $ liftIO $ do
             Pandoc.writeFile htmlPostPath htmlPost
@@ -210,15 +294,108 @@ main = shakeArgs shakeOptions{shakeFiles = "docs"} $ do
 
 -- * Instances
 
+-- | 'PandocShake' is essentially 'PandocIO' with the following differences
+--
+--      - shake's 'Action' monad is at the bottom of the monad transformer
+--      stack instead of IO
+--
+--      - 'lookupEnv' lookups environment variables in the provided environment
+--      instead of consulting the process's environment variable. This is because 
+newtype PandocShake a = PandocShake 
+    { unPandocShake :: 
+        ExceptT PandocError (ReaderT [(Text,Text)] (StateT CommonState Action)) a
+    } deriving
+        ( MonadIO
+        , Functor
+        , Applicative
+        , Monad
+        , MonadError PandocError
+        )
+
+-- | @'runPandocShakeWithEnvOrExplode' env pandocShake @ runs the 
+-- @pandocShake :: 'PandocShake'@ with @env@ as the environment variables, and
+-- returns the resulting shake 'Action'.
+runPandocShakeWithEnvOrExplode :: [(Text, Text)] -> PandocShake a -> Action a
+runPandocShakeWithEnvOrExplode env pandocShake = do
+    result <- State.evalStateT (Reader.runReaderT (Except.runExceptT (unPandocShake pandocShake)) env) Pandoc.def 
+    liftIO $ Pandoc.handleError result
+
+instance PandocMonad PandocShake where
+    lookupEnv envVar = PandocShake 
+        $ Trans.lift 
+        $ Reader.asks 
+        $ lookup envVar
+    getCurrentTime = liftPandocIOToPandocShake Pandoc.getCurrentTime
+    getCurrentTimeZone = liftPandocIOToPandocShake Pandoc.getCurrentTimeZone
+    newStdGen = liftPandocIOToPandocShake Pandoc.newStdGen
+    newUniqueHash = liftPandocIOToPandocShake Pandoc.newUniqueHash
+    openURL = liftPandocIOToPandocShake . Pandoc.openURL
+
+    readFileStrict filePath = do
+        file <- liftPandocIOToPandocShake $ Pandoc.readFileStrict filePath
+        -- Note [Why we 'need' after reading the file]
+        -- Internally, in 'Text.Pandoc.Class.PandocMonad' there are functions
+        -- like 'Text.Pandoc.Class.PandocMonad.readFileFromDirs' which _rely_
+        -- on the fact that 'Text.Pandoc.readFileStrict' throws exceptions when
+        -- a file is not found.
+        -- This is quite unfortunate as this is essentially contrary to the
+        -- functionality that shake provides.
+        --
+        -- TODO: perhaps this is worth filing an issue in pandocs to move this
+        -- functionality out?
+        liftActionToPandocShake $ need [filePath]
+        return file
+    readFileLazy filePath = do
+        file <- liftPandocIOToPandocShake $ Pandoc.readFileLazy filePath
+        -- See: Note [Why we 'need' after reading the file]
+        liftActionToPandocShake $ need [filePath]
+        return file
+
+    readStdinStrict = liftPandocIOToPandocShake $ Pandoc.readStdinStrict
+    glob globPattern = do
+        files <- liftPandocIOToPandocShake $ Pandoc.glob globPattern
+        liftActionToPandocShake $ need files
+        return files
+    fileExists filePath = do
+        -- TODO: is this right? if it doesn't exist, then shake will probably
+        -- complain about it before pandoc can get mad
+        liftActionToPandocShake $ need [filePath]
+        liftPandocIOToPandocShake $ Pandoc.fileExists filePath
+    getDataFileName filePath = do
+        -- TODO: is this right? do we 'need' the path given OR the path after
+        -- it is given? this needs to be investigated more..
+        filePath' <- liftPandocIOToPandocShake $ Pandoc.getDataFileName filePath
+        liftActionToPandocShake $ need [filePath']
+        return filePath'
+    getModificationTime filePath = do
+        liftActionToPandocShake $ need [filePath]
+        utcTime <- liftPandocIOToPandocShake $ Pandoc.getModificationTime filePath
+        return utcTime
+    getCommonState = liftPandocIOToPandocShake $ Pandoc.getCommonState
+    putCommonState st = liftPandocIOToPandocShake $ Pandoc.putCommonState st
+    logOutput = liftPandocIOToPandocShake . Pandoc.logOutput
+
+-- | lifts a 'Action' into a 'PandocShake'
+liftActionToPandocShake :: Action a -> PandocShake a
+liftActionToPandocShake ma = PandocShake $ Trans.lift $ Trans.lift $ Trans.lift ma
+
+-- | lifts a 'PandocIO' into a 'PandocShake'
+liftPandocIOToPandocShake :: PandocIO a -> PandocShake a 
+liftPandocIOToPandocShake ma = PandocShake $ do
+    st <- Trans.lift $ Trans.lift State.get
+    (a, st') <- liftIO $ State.runStateT (Except.runExceptT (unPandocIO ma)) st
+    Trans.lift $ Trans.lift $ State.put st'
+    Except.liftEither a
+
 -- | 'TemplateAction' is a newtype wrapper around 'Action' to provide and
 -- instance for 'DocTemplates.TemplateMonad' which automatically will 'need' partial
 -- templates from pandocs.
 newtype TemplateAction a = TemplateAction { runTemplateAction :: Action a }
   deriving (Functor, Applicative, Monad, MonadIO)
 
--- | 'liftAction' lifts an 'Action' into a 'TemplateAction'
-liftAction :: Action a -> TemplateAction a
-liftAction = TemplateAction
+-- | 'liftActionToTemplateAction' lifts an 'Action' into a 'TemplateAction'
+liftActionToTemplateAction :: Action a -> TemplateAction a
+liftActionToTemplateAction = TemplateAction
 
 instance DocTemplates.TemplateMonad TemplateAction where
-    getPartial filePath = liftAction $ need [filePath] >> liftIO (Pandoc.readFile filePath)
+    getPartial filePath = liftActionToTemplateAction $ need [filePath] >> liftIO (Pandoc.readFile filePath)
